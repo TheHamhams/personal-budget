38eebed5ed0e3d1f63bc1f29abc09868cc1fb3eb
Corey <hamhams86@gmail.com>
2022-07-13 10:41:43 -0700

    added envelopes, totalBudget, and ammountBudgeted

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser')
 
+module.exports = app
+
 const PORT = process.env.PORT || 4001
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send('Ello Govna')
+let totalBudget = 0
+let ammountBudgeted = 0
+let envelopes = [
+    {food: {items: [], budget: 0, idCount: 0}},
+    {fun: {items: [], budget: 0, idCount: 0}},
+    {savings: {items: [], budget: 0, idCount: 0}},
+    {upkeep: {items: [], budget: 0, idCount: 0}}
+]
+
+const apiRouter = express.Router()
+app.use('/envelopes', apiRouter)
+
+
+apiRouter.get('/', (req, res, next) => {
+    res.send(envelopes)
 })
-
-
-
 
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
